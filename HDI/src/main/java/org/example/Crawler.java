4fad546d2fc79cf147b9632bca5ad2c7e0dee628
shivam0rawat0 <shivam100006rawat@gmail.com>
2023-06-26 00:57:50 +0530

    added support for pages and url exclusion

--- HDI/src/main/java/org/example/Crawler.java
+++ HDI/src/main/java/org/example/Crawler.java
@@ -1,16 +1,15 @@
 package org.example;
 
+import java.io.*;
 import java.util.ArrayDeque;
 import java.util.HashSet;
 import java.util.Queue;
 import java.util.Set;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 public class Crawler {
-    public static Set<String> Restricted = new HashSet();
+    public static Set<String> ExcludedPage = new HashSet();
+    public static Set<String> ExcludedURL = new HashSet();
     public static String header = "_________________________________________________________________________";
     public Set<String> linkSet;
     public Queue<String> linkQueue;
@@ -19,21 +18,27 @@ public class Crawler {
     public int DEPTH;
     public int LINK_COUNT;
     public Crawler() {
-        Restricted.add("png");
-        Restricted.add("jpeg");
-        Restricted.add("jpg");
-        Restricted.add("gif");
-        Restricted.add("js");
-        Restricted.add("json");
-        Restricted.add("webm");
-        Restricted.add("mp3");
-        Restricted.add("mp4");
-        Restricted.add("wav");
-        Restricted.add("aud");
-        Restricted.add("css");
+        loadResource("excluded-pages.txt",ExcludedPage);
+        loadResource("excluded-urls.txt",ExcludedURL);
         linkSet = new HashSet<String>();
         linkQueue = new ArrayDeque<String>();
         breadthQueue = null;
+    }
+
+    public void loadResource(String fileName,Set<String> resource){
+        InputStream ioStream = this.getClass()
+                .getClassLoader()
+                .getResourceAsStream(fileName);
+        if (ioStream == null) {
+            throw new IllegalArgumentException(fileName + " is not found");
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(ioStream));
+            String line;
+            while ((line = reader.readLine()) != null)
+                resource.add(line);
+            reader.close();
+        } catch(Exception e){}
     }
 
     public void setSeed(String seed){
@@ -51,7 +56,6 @@ public class Crawler {
 
     public void dfs(String groupName,int breadth, int depth){
         if(depth<DEPTH) {
-            Queue<String> temp = null;
             int gid = 0;
             System.out.println(header+"Level["+depth+"]"+header);
             while (!linkQueue.isEmpty()) {
@@ -81,36 +85,64 @@ public class Crawler {
                     StringBuilder response = new StringBuilder();
                     while ((line = reader.readLine()) != null) response.append(line);
                     reader.close();
+                    connection.disconnect();
                     line = response.toString();
                     int startIndex = 0;
-                    while (true) {
-                        startIndex = beginIndex(line,startIndex);
+                    while (breadth>0) {
+                        /*startIndex = beginIndex(line,startIndex);
                         if (startIndex < 0) break;
                         int endIndex = lastIndex(line,startIndex);
                         if (endIndex < 0) break;
-
-                        String link = line.substring(startIndex, endIndex);
-                        int li = link.lastIndexOf(".");
-                        if(li==-1){
-                            if (--breadth < 0) break;
-                            else {breadthQueue.add(link);++linkCount;}
-                        } else {
-                            if(!Restricted.contains(link.substring(li+1))) {
-                                if (--breadth < 0) break;
-                                else {breadthQueue.add(link);++linkCount;}
+                        String link = line.substring(startIndex, endIndex);*/
+                        String URL = extractURL(line,startIndex);
+                        if(URL==null){
+                            --breadth;
+                            continue;
+                        }
+                        String index [] = URL.split(",");
+                        String link = line.substring(Integer.valueOf(index[0]), Integer.valueOf(index[1]));
+                        System.out.println(link);
+                        if(!link.equals("") && (link.charAt(0)!='/' && link.charAt(0)!='#') && link.charAt(0)=='h'){
+                            String domain;
+                            int s = link.indexOf("//"),e = link.indexOf("/",s+2);
+                            if(e==-1) domain = link.substring(s+2);
+                            else domain = link.substring(s+2,link.indexOf("/",s+2));
+                            if(!linkSet.contains(link) && !ExcludedURL.contains(domain)){
+                                ExcludedURL.add(domain);
+                                int li = link.lastIndexOf(".");
+                                if(li==-1){
+                                    breadthQueue.add(link);
+                                    ++linkCount;
+                                } else {
+                                    if(!ExcludedPage.contains(link.substring(li+1))) {
+                                        breadthQueue.add(link);
+                                        ++linkCount;
+                                    }
+                                }
                             }
                         }
-                        startIndex = endIndex + 1;
+                        startIndex = Integer.valueOf(index[1]) + 1;
                     }
                 }
-                connection.disconnect();
                 System.out.println("Group["+depth+groupName+"][" + linkCount + "]["+inputUrl+"]");
             } catch (IOException e) {
+                e.printStackTrace();
                 System.out.println("Group["+depth+groupName+"][error]["+inputUrl+"]");
             }
         }
     }
 
+    public static String extractURL(String line,int startIndex){
+        int start = line.indexOf("<a", startIndex);
+        if(start==-1) return null;
+        int end = line.indexOf(">", start);
+        if(end==-1) return null;
+        int http = line.indexOf("href=",start);
+        if(http==-1) return null;
+        int q1 = line.indexOf("\"",http);
+        int q2 = line.indexOf("\"",q1+1);
+        return (q1+1)+","+q2;
+    }
     public static int beginIndex(String line,int startIndex){
         int http = line.indexOf("http://", startIndex);
         int https = line.indexOf("https://", startIndex);
--- HDI/src/main/java/org/example/Main.java
+++ HDI/src/main/java/org/example/Main.java
@@ -3,9 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Crawler crawler = new Crawler();
-        crawler.setSeed("Link to be seeded initially");
+        crawler.setSeed("https://example.com");
         //breadth, depth and number of links to be processed
-        crawler.setLimit(10,20, 1000);
+        crawler.setLimit(200,100, 100000);
         crawler.crawl();
     }
 }

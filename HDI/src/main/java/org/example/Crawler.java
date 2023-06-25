b9933f5d9a9518bea3f64e4a1a8b2f20097105a4
shivam0rawat0 <shivam100006rawat@gmail.com>
2023-06-25 19:13:57 +0530

    added support for improved URL scraping from response

--- HDI/src/main/java/org/example/Crawler.java
+++ HDI/src/main/java/org/example/Crawler.java
@@ -10,14 +10,14 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 public class Crawler {
+    public static Set<String> Restricted = new HashSet();
+    public static String header = "_________________________________________________________________________";
     public Set<String> linkSet;
     public Queue<String> linkQueue;
+    public Queue<String> breadthQueue;
     public int BREADTH;
     public int DEPTH;
     public int LINK_COUNT;
-
-    public static Set<String> Restricted = new HashSet();
-
     public Crawler() {
         Restricted.add("png");
         Restricted.add("jpeg");
@@ -30,8 +30,10 @@ public class Crawler {
         Restricted.add("mp4");
         Restricted.add("wav");
         Restricted.add("aud");
+        Restricted.add("css");
         linkSet = new HashSet<String>();
         linkQueue = new ArrayDeque<String>();
+        breadthQueue = null;
     }
 
     public void setSeed(String seed){
@@ -44,60 +46,89 @@ public class Crawler {
         LINK_COUNT = count;
     }
     public void crawl(){
-      dfs(BREADTH,0);
+        dfs("",BREADTH,0);
     }
 
-    public void dfs(int breadth, int depth){
+    public void dfs(String groupName,int breadth, int depth){
         if(depth<DEPTH) {
+            Queue<String> temp = null;
+            int gid = 0;
+            System.out.println(header+"Level["+depth+"]"+header);
             while (!linkQueue.isEmpty()) {
-                process(breadth,depth + 1, linkQueue.poll());
-                dfs(breadth, depth + 1);
+                process(groupName +"-"+Integer.toString(++gid),breadth,depth, linkQueue.poll());
+            }
+            if(breadthQueue!=null) {
+                linkQueue = breadthQueue;
+                breadthQueue = null;
+                dfs(groupName, breadth, depth + 1);
             }
         }
     }
-    public void process(int breadth,int depth,String inputUrl) {
+
+    public void process(String groupName,int breadth,int depth,String inputUrl) {
         if(!linkSet.contains(inputUrl) && linkSet.size()<LINK_COUNT) {
-            System.out.println("at [" + depth + "]" + inputUrl);
             linkSet.add(inputUrl);
+            if(breadthQueue==null) breadthQueue = new ArrayDeque<String>();
+            int linkCount = 1;
             try {
                 URL url = new URL(inputUrl);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("GET");
                 int responseCode = connection.getResponseCode();
                 if (responseCode == 200) {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                     String line;
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                     StringBuilder response = new StringBuilder();
                     while ((line = reader.readLine()) != null) response.append(line);
-
+                    reader.close();
                     line = response.toString();
                     int startIndex = 0;
                     while (true) {
-                        int http = line.indexOf("http://", startIndex);
-                        int https = line.indexOf("https://", startIndex);
-                        if(http == -1){
-                            if(https == -1) return;
-                            else startIndex = https;
-                        } else if(https == -1) {
-                            startIndex = http;
-                        } else startIndex = Math.min(http,https);
+                        startIndex = beginIndex(line,startIndex);
                         if (startIndex < 0) break;
-                        int endIndex = line.indexOf("\"",startIndex);
+                        int endIndex = lastIndex(line,startIndex);
                         if (endIndex < 0) break;
-                        String link = line.substring(startIndex, endIndex);//.split(" ")[0];
-                        startIndex = endIndex + 1;
-                        if(!Restricted.contains(line.substring(line.lastIndexOf("[.]"))+1)) {
-                            if (--breadth < 0) return;
-                            linkQueue.add(link);
+
+                        String link = line.substring(startIndex, endIndex);
+                        int li = link.lastIndexOf(".");
+                        if(li==-1){
+                            if (--breadth < 0) break;
+                            else {breadthQueue.add(link);++linkCount;}
+                        } else {
+                            if(!Restricted.contains(link.substring(li+1))) {
+                                if (--breadth < 0) break;
+                                else {breadthQueue.add(link);++linkCount;}
+                            }
                         }
+                        startIndex = endIndex + 1;
                     }
-                    reader.close();
                 }
                 connection.disconnect();
+                System.out.println("Group["+depth+groupName+"][" + linkCount + "]["+inputUrl+"]");
             } catch (IOException e) {
-                System.out.println("error");
+                System.out.println("Group["+depth+groupName+"][error]["+inputUrl+"]");
             }
         }
+    }
+
+    public static int beginIndex(String line,int startIndex){
+        int http = line.indexOf("http://", startIndex);
+        int https = line.indexOf("https://", startIndex);
+        if(http == -1){
+            if(https == -1) return https;
+            return https;
+        } else if(https == -1) {
+            return http;
+        } return Math.min(http,https);
+    }
+
+    public static int lastIndex(String line,int startIndex){
+        int co = Math.min(line.indexOf(" ",startIndex),line.indexOf("<",startIndex));
+        co = Math.min(co,line.indexOf("*",startIndex));
+        co = Math.min(co,line.indexOf(";",startIndex));
+        co = Math.min(co,line.indexOf("'",startIndex));
+        co = Math.min(co,line.indexOf("\\",startIndex));
+        return Math.min(line.indexOf("\"",startIndex),co);
     }
 }
 
--- HDI/src/main/java/org/example/Main.java
+++ HDI/src/main/java/org/example/Main.java
@@ -3,8 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Crawler crawler = new Crawler();
-        crawler.setSeed("https://en.wikipedia.org/wiki/Main_Page");
-        crawler.setLimit(20,2, 200);//Breadth and Depth
+        crawler.setSeed("Link to be seeded initially");
+        //breadth, depth and number of links to be processed
+        crawler.setLimit(10,20, 1000);
         crawler.crawl();
     }
 }
